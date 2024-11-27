dd5bdb55c570f906e673e62e457f287a6cb998d2
Sarah Haddad <69437567+sxh01@users.noreply.github.com>
2024-11-27 22:15:26 +0300

    Update FitnessRecommendationSystem.java

--- FitnessRecommendationSystem.java
+++ FitnessRecommendationSystem.java
@@ -18,12 +18,12 @@ public class FitnessRecommendationSystem {
             while (!signupSuccess) {
                 String username = getInput(scanner, "new username");
                 String password = getInput(scanner, "new password");
-                String role = getInput(scanner, "your role (Member)");
+                String role = getInput(scanner, "your role (Member/Admin)");
 
-                if (!"Member".equalsIgnoreCase(role)) {
+              /*  if (!"Member".equalsIgnoreCase(role)) {
                     System.out.println("Invalid role. Only 'Member' role is allowed.");
                     continue;
-                }
+                }*/
 
                 System.out.println("Enter additional details to complete the sign-up process:");
                 int age = Integer.parseInt(getInput(scanner, "Age"));
@@ -43,20 +43,69 @@ public class FitnessRecommendationSystem {
             return;
         }
 
+        if (user.getRole().equalsIgnoreCase("Admin")){
+            System.out.println("Welcome to admin panel");
+            System.out.println("1. Edit users\n2. Exit");
+            int choice;
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            if(choice==1) {
+                System.out.println("Enter username: ");
+                String userName = scanner.nextLine();
+                authService.deleteUser(userName);
+                System.exit(0);
+            } else if (choice ==2) {
+                System.exit(0);
+            }
+        }
+
         // Collect fitness preferences
         System.out.println("Please provide additional details for personalized fitness recommendations:");
-        System.out.print("Enter your primary fitness goal (e.g., Weight Loss, Muscle Building, Improve Flexibility, Stress Relief): ");
-        String fitnessGoal = scanner.nextLine();
-        System.out.print("Enter your fitness level (Beginner, Intermediate, Advanced): ");
-        String fitnessLevel = scanner.nextLine();
+        System.out.print("Enter your primary fitness goal\n1. Weight Loss \n2. Muscle Building\n3. Improve Flexibility\n4. Stress Relief:\n");
+        int fitnessGoal = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (fitnessGoal){
+            case 1:
+                user.setFitnessGoal(FitnessPlan.FitnessGoal.Weight_Loss);
+                break;
+            case 2:
+                user.setFitnessGoal(FitnessPlan.FitnessGoal.Muscle_Building);
+                break;
+            case 3:
+                user.setFitnessGoal(FitnessPlan.FitnessGoal.Improve_Flexibility);
+                break;
+            case 4:
+                user.setFitnessGoal(FitnessPlan.FitnessGoal.Stress_Relief);
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+
+        System.out.print("Choose your fitness level\n1. Beginner\n2. Intermediate\n3. Advanced\n");
+        int fitnessLevel = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (fitnessLevel){
+            case 1:
+                user.setFitnessLevel(FitnessPlan.FitnessLevel.Beginner);
+                break;
+            case 2:
+                user.setFitnessLevel(FitnessPlan.FitnessLevel.Intermediate);
+                break;
+            case 3:
+                user.setFitnessLevel(FitnessPlan.FitnessLevel.Advanced);
+                break;
+            default:
+                System.out.println("Invaild choice");
+        }
+
         System.out.print("Do you have any illnesses (e.g., High blood pressure, Joint issues, None)? ");
         String illnesses = scanner.nextLine();
         System.out.print("Do you have any surgeries (e.g., Minor surgeries, Surgeries in the past year, None)? ");
         String surgeries = scanner.nextLine();
 
         // Set user preferences
-        user.setFitnessGoal(fitnessGoal);
-        user.setFitnessLevel(fitnessLevel);
         user.setIllnesses(illnesses);
         user.setSurgeries(surgeries);
 
